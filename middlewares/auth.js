c3722003ae568150006cca26945771529e6d8bd5
Jithin458 <jithinmp45@gmail.com>
2025-06-18 00:51:14 +0530

    added ftn parameters

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 
-const verify = ()=>{
+const verifyToken = (req,res,next)=>{
 const token = req.headers.authorization?.split(" ")[1]
 if(!token){
     res.status(401).json({msg:"No token provided"})
 }
 try{
-
-}catch(err){
-    console.log(err);
-    res.status().json(err)
-}
 const decoded = jwt.verify(token,"secret");
 req.hostEmail = decoded.email;
+next()
+}catch(err){
+    console.log(err);
+    res.status(403).json(err)
+}}
 
-}
+module.exports = verifyToken();
