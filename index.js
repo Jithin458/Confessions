1535c964a40feace1db9f7d73d0f665c66cd5309
Jithin458 <jithinmp45@gmail.com>
2025-06-23 21:46:13 +0530

    implemented logging

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const mongoose = require("mongoose");
 const hostRouter = require("./routes/host");
 const userRouter = require("./routes/users");
 const helmet = require("helmet");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
+const logStream = fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'});
 
 app.use(helmet());
+app.use(morgan(':method :url :date[web]',{stream:logStream}));
 app.use(express.json());
 app.use("/host",hostRouter);
 app.use("/user",userRouter);
